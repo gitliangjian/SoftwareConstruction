971ecdce0b014a3308a4007fc2d9321f68bb6c43
gitliangjian <153593212+gitliangjian@users.noreply.github.com>
2025-02-24 19:46:03 +0800

    第一次作业

---
+++ assignment1/WindowsFormCalculator/WindowsFormCalculator/CalculatorForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsCalculator
+{
+    partial class CalculatorForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNumber1.Location = new System.Drawing.Point(20, 19);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(120, 30);
+            this.txtNumber1.TabIndex = 0;
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.Location = new System.Drawing.Point(160, 19);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(60, 33);
+            this.cmbOperator.TabIndex = 1;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNumber2.Location = new System.Drawing.Point(240, 19);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(120, 30);
+            this.txtNumber2.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCalculate.Location = new System.Drawing.Point(20, 66);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(340, 38);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "计算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblResult.Location = new System.Drawing.Point(20, 122);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(340, 56);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 190);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "CalculatorForm";
+            this.Text = "计算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.ComboBox cmbOperator;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
