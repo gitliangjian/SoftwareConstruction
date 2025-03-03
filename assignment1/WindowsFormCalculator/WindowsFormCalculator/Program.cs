952e2c57295e2edc1910cb50c4bf61acf81532e6
gitliangjian <153593212+gitliangjian@users.noreply.github.com>
2025-03-03 23:17:14 +0800

    第一次作业

---
+++ assignment1/WindowsFormCalculator/WindowsFormCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormCalculator;
+
+namespace WindowsFormsCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CalculatorForm()); // 这里指定启动窗体
+        }
+    }
+}
