5c500573fce989a71e3fed56bcddec63bb399332
gitliangjian <153593212+gitliangjian@users.noreply.github.com>
2025-03-14 14:34:28 +0800

    第四次作业

---
+++ assignment4/Clock/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{   
+    // 委托类型
+    public delegate void TickEventHandler(object sender, EventArgs e);
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+    // 闹钟类
+    class AlarmClock
+    {
+        private DateTime alarmTime;
+        public event TickEventHandler Tick;
+        public event AlarmEventHandler Alarm;
+        //响铃时间
+        public AlarmClock(DateTime alarmTime)
+        {
+            this.alarmTime = alarmTime;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                OnTick(now);
+
+                if (now >= alarmTime)
+                {
+                    OnAlarm();
+                    break;
+                }
+                Thread.Sleep(1000);//每秒检查
+            }
+        }
+
+        protected virtual void OnTick(DateTime currentTime)
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // 设置响铃时间为五秒钟后
+        DateTime alarmTime = DateTime.Now.AddSeconds(5);
+        AlarmClock clock = new AlarmClock(alarmTime);
+
+        // 订阅事件
+        clock.Tick += (sender, e) =>
+        {
+            Console.SetCursorPosition(0, Console.CursorTop); // 将光标移到行首
+            Console.Write($"当前时间: {DateTime.Now:HH:mm:ss}    "); // 覆盖当前行，末尾加空格清除残留字符
+        };
+        clock.Alarm += (sender, e) => Console.WriteLine("\n响铃！时间到！"); // 换行显示响铃
+
+        Console.WriteLine($"闹钟设置为: {alarmTime:HH:mm:ss}");
+        clock.Start();
+    }
+}
---
+++ assignment4/GenericList/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace GenericList
+{
+    //泛型链表类和结点
+    //链表结点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T data)
+        {
+            Next = null;
+            Data = data;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList(Node<T> head, Node<T> tail)
+        {
+            this.head = head;
+            this.tail = tail;
+        }
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail ==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(5);
+            list.Add(3);
+            list.Add(9);
+            list.Add(2);
+
+            // 打印链表元素
+            Console.WriteLine("Elements:");
+            list.ForEach(x => Console.WriteLine(x));
+
+            // 求最大值
+            int max = int.MinValue;
+            list.ForEach(x => max = x > max ? x : max);
+            Console.WriteLine($"Max: {max}");
+
+            // 求最小值
+            int min = int.MaxValue;
+            list.ForEach(x => min = x < min ? x : min);
+            Console.WriteLine($"Min: {min}");
+
+            // 求和
+            int sum = 0;
+            list.ForEach(x => sum += x);
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
